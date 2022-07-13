506a3888099a10abf45ed62cb7d160fbff1ee384
stas <stanislavkhizhnyak1996@gmail.com>
2022-07-13 23:22:12 +0300

    added Cacheable

--- src/main/java/com/example/demosp/DemoSpApplication.java
+++ src/main/java/com/example/demosp/DemoSpApplication.java
@@ -2,12 +2,18 @@ package com.example.demosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoSpApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpApplication.class, args);
 	}
 
+	@Scheduled(cron = "0 0 * * * *")
+	@CacheEvict(value = "photo")
+	public void clearCash(){};
 }
--- src/main/java/com/example/demosp/pictures.java
+++ src/main/java/com/example/demosp/pictures.java
@@ -1,7 +1,10 @@
 package com.example.demosp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,13 @@ public class pictures {
     @GetMapping("/pictures/{sol}/largest")
     public ResponseEntity getLargestImage(@PathVariable int sol) {
         String url = String.format("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=%d&api_key=DEMO_KEY", sol);
+        String largestUrl = getLargestUrl(url);
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(URI.create(largestUrl)).build();
+    }
+
+
+    @Cacheable("photo")
+    public String getLargestUrl(String url) {
         RestTemplate restTemplate = new RestTemplate();
         JsonNode forEntity = restTemplate.getForObject(url, JsonNode.class);
         long imageLength = 0;
@@ -30,6 +40,6 @@ public class pictures {
                 largestUrl = img_src;
             }
         }
-        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(URI.create(largestUrl)).build();
+        return largestUrl;
     }
 }
